863d20ba1d0ac61e2852ce718747928890539e5a
Patrick Gallagher <paddy.gallagher@gmail.com>
2015-11-16 20:53:43 +0000

    Initial project setup

---
+++ test/concurrent_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns concurrent-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [concurrent-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
